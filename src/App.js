c9d0c847554d4cf330951d07b59efcc3963e0c0f
brett-burley <brettdburley@gmail.com>
2019-11-27 15:29:16 -0800

    updated menu-items to navigate to proper URL with react-router-dom module

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+
+const Hats = (props) => {
+  console.log(props);
+  return(
+    <div>
+      <h1>Dam lots of Hats</h1>
+    </div>
+  );
+  };
 
 function App() {
   return (
     <div>
-      <HomePage />
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={Hats} />
     </div>
   );
 }
