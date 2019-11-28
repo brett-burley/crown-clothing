29925dc59dd2b80717f1834963fa0cc6b071f7c6
brett-burley <brettdburley@gmail.com>
2019-11-28 11:07:10 -0800

    finished the shop page, collection-preview component and collection-item component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 
-const Hats = (props) => {
-  console.log(props);
-  return(
-    <div>
-      <h1>Dam lots of Hats</h1>
-    </div>
-  );
-  };
 
 function App() {
   return (
     <div>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={Hats} />
+        <Route exact path='/shop' component={ShopPage} />
     </div>
   );
 }
