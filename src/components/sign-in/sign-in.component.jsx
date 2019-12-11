8caaf9361646a47331db499497271614ef3e86ef
brett-burley <brettdburley@gmail.com>
2019-12-11 14:51:22 -0800

    finished sign-in and sign-up components

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -17,17 +17,24 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = e => {
+    handleSubmit = async e => {
         e.preventDefault();
 
-        this.setState({ email: '', password: '' })
-    }
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: ''});
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     handleChange = e => {
         const { value, name } = e.target;
 
         this.setState({ [name]: value });
-    }
+    };
 
     render() {
         return(
@@ -49,7 +56,7 @@ class SignIn extends React.Component {
                         name="password" 
                         type="password" 
                         handleChange={this.handleChange} 
-                        value={this.state.email} 
+                        value={this.state.password} 
                         required
                         label="Password" 
                     />
